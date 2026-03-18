91b0bbb1786c5192cc5f67aec6797cb3a577cb7f
ucup774 <rajacupu02@gmail.com>
2026-03-19 03:26:04 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ apt update && apt upgrade -y
 apt install python3 dropbear -y
 
 # Download script menu dari github kamu sendiri
-wget -O /usr/bin/menu "https://raw.githubusercontent.com/USERNAME_KAMU/my-vps-script/main/menu.sh"
+wget -O /usr/bin/menu "https://raw.githubusercontent.com/ucup774/my-vps-script/main/menu.sh"
 chmod +x /usr/bin/menu
 
 echo "Instalasi selesai. Ketik 'menu' untuk mengelola user."
